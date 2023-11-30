f80d16c3b312b3d2ebcab05d771ae38b3c846ab6
melgiler <146897278+melgiler@users.noreply.github.com>
2023-11-30 16:25:48 -0500

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,3 +1,43 @@
 public class Vehicle {
-    
+	private int ownerId;
+    private VehicleOwner requester;
+    private boolean completed;
+
+    public Vehicle(int ownerId, VehicleOwner requester, boolean completed) {
+        this.ownerId = ownerId;
+        this.requester = requester;
+        this.completed = completed;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public VehicleOwner getRequester() {
+        return requester;
+    }
+
+    public void setRequester(VehicleOwner requester) {
+        this.requester = requester;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    //Add functionality
+    public void start() {}
+
+    //Added functionality
+    public void transferToServer(Server s) {}
+
+
 }
